10cd47f6893c372f9c4fdd44195ce79c860ef4bf
p1412480 <p1412480@EPULWKS60.univ-lyon1.fr>
2015-05-28 09:57:46 +0200

    Ok

--- src/servicetransfertfichier/receive/Receive.java
+++ src/servicetransfertfichier/receive/Receive.java
@@ -20,15 +20,15 @@ public class Receive {
     private DatagramPacket dps, dpr;
     private InetAddress ad;
     private int port = 69;
-    private FileInputStream f;
-    private boolean isReceiving;
+    private FileOutputStream f;
+    private boolean isReceiving = true;
       
     public int receiveFile(String localName, String remoteName, String address){
         
         try {
             ad = InetAddress.getByName(address);
             ds = new DatagramSocket();
-            f = new FileInputStream(localName);
+            f = new FileOutputStream(localName);
         }
         catch (UnknownHostException ex)     {Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);}
         catch (SocketException ex)          {Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);}
@@ -43,10 +43,11 @@ public class Receive {
             msg[i] = remoteName.getBytes()[i-2];
         msg[2+lgt] = 0x00;
         int lgtMode = "octet".getBytes().length;
-        for(int i = 3+lgt; i < lgtMode+3+lgt; i++)
-            msg[i] = "octet".getBytes()[i-2];
+        for(int i = 3+lgt; i < lgtMode+3+lgt ; i++)
+            msg[i] = "octet".getBytes()[i-lgt-3];
         msg[lgtMode+3+lgt] = 0x00;
         /*Fin header*/
+        
         
         dps = new DatagramPacket(msg, msg.length, ad, port);
         byte[] data = new byte[516];
@@ -58,19 +59,72 @@ public class Receive {
         }
         catch (IOException ex) {Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);}
         
-        int nbBlock = 0;
+        int nbBlock = 1;
+       
         while(isReceiving){
+            
+            port = dpr.getPort();
+            
             if(data[0] != 0x00 || data[1] != 0x03){
                 System.err.println("Echec de la réception");
                 return 1;
             }
-//            if(nbBlock != data)
-
+            
+            //TODO : Gérer les n°blocks au delà de 127. Num bloc est seulement data[3], data[2] toujours vide !
+            else if(nbBlock != data[3]){
+                System.out.println("Bloc déjà reçu :"+nbBlock+" data :"+data[3]);
+                sendAck(data[3]);
+                data = new byte[516];
+                dpr = new DatagramPacket(data, data.length);
+                try {ds.receive(dpr);}
+                catch (IOException ex) {
+                    Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            else {
+                //TODO : Lever l'entête
+                try { f.write(dpr.getData()); }
+                catch (IOException ex) {
+                    Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println(data[3]);
+                sendAck(data[3]);
+                nbBlock = data[3]+1;
+                System.out.println(dpr.getData().length);
+                if (dpr.getData().length < 516){
+                    isReceiving = false;
+                    System.out.println("Bloc d'arrêt");
+                }
+                else{
+                    System.out.println("Bloc 512 : On continue");
+                    data = new byte[516];
+                    dpr = new DatagramPacket(data, data.length);
+                    try {ds.receive(dpr);}
+                    catch (IOException ex) {
+                        Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
             
         }
         
-        
         return 0;
+    }
+    
+    public void sendAck(int numBlock){
+        byte[] ack = new byte[4];
+        ack[0] = 0x00;
+        ack[1] = 0x04;
+        ack[2] = 0x00;
+        ack[3] = (byte)numBlock;
+        
+        dps = new DatagramPacket(ack, ack.length, ad, port);
+        
+        try { ds.send(dps); }
+        catch (IOException ex) {
+            Logger.getLogger(Receive.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
